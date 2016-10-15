08a39652283a2cf7e45ced9f7d1d8fe911c06d96
Stefani Castellanos <scct95@hotmail.com>
2016-10-15 17:33:21 -0400

    heuristica manhattan sin probar ni compilar. Idea..

--- psvn-for-ci5437/global/manhattan_heuristic.hpp
+++ psvn-for-ci5437/global/manhattan_heuristic.hpp
@@ -7,3 +7,20 @@
 
     This file contains the implementation for manhattan distance heuristic
 */
+
+#include <vector>
+
+int manhattan_heuristic(state_t *state) {
+
+    int cost = 0;
+    int statei;
+    div_t divresult;
+    divresult = div (i - statei,n);
+
+    //iterate through state's coordenates
+    for (int i = 0; i < NUMVARS - 1; i++) {
+        statei = state -> vars[i];
+        cost += divresult.quot + divresult.rem;
+    }
+    return cost;
+}
