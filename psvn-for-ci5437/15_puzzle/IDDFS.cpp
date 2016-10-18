68feccad5f9334cc39d9e3697694fac109de294a
Edward Fernandez <edwardfernandez26@gmail.com>
2016-10-17 20:30:21 -0400

    Listo el 15_puzzle.psvn y el IDDFS.cpp

--- psvn-for-ci5437/15_puzzle/IDDFS.cpp
+++ psvn-for-ci5437/15_puzzle/IDDFS.cpp
@@ -8,27 +8,32 @@
 
 using namespace std;
 
+// Let you convert an integer to string.
 string convertInt(int number){
   ostringstream ss; // Create a stringstream.
   ss << number;     // Add number to the stream
   return ss.str();  // Return a string with the content of the stream.
 }
 
+// Let you obtain the total nodes on the actual label.
 int64_t bounded_dfs_visit(state_t state, int deep, int bound, int history){
-  int64_t numNodoAct = 0;
   int ruleid;
   state_t child;
+  int64_t numNodoAct = 0;
   ruleid_iterator_t iter;
 
-  if (deep == bound - 1) return 1;
+  if (deep == 0) return 0;
   else{
 
-    init_fwd_iter(&iter, &state);
+    init_bwd_iter(&iter, &state);
+
     while( (ruleid = next_ruleid(&iter)) >= 0 ){
-      if (fwd_rule_valid_for_history( history, ruleid) != 0){
-        int nextHistory = next_fwd_history(history, ruleid);
-        apply_fwd_rule(ruleid, &state, &child);
-        numNodoAct = bounded_dfs_visit(state, deep + 1, bound, nextHistory);
+
+      if (bwd_rule_valid_for_history(history, ruleid) != 0){
+        apply_bwd_rule(ruleid, &state, &child);
+        int nextHistory = next_bwd_history(history, ruleid);
+        int64_t totalAux = bounded_dfs_visit(child, deep - 1, bound, nextHistory);
+        numNodoAct += totalAux;
       }
     }
   }
@@ -36,23 +41,36 @@ int64_t bounded_dfs_visit(state_t state, int deep, int bound, int history){
   return numNodoAct + 1;
 }
 
+// Let you use the Iterative Deepening DFS.
 void iterative_deepening_depth_first_search()
 {
-  state_t state;
-  int goal_num;  // ID of the goal condition.
-  int bound = 11; // The deep bound.
-  int64_t totalNodes = 0;
-  int64_t oldTotalNodes = 0;
+  state_t state;                // Actual state.
+  int goal_num;                 // ID of the goal condition.
+  int bound;                    // The deep bound.
+  int64_t totalNodes = 0;       // Total nodes on the actual bound.
+  int64_t oldTotalNodes = 0;    // Total nodes on the last bound.
+  int64_t *array;               // Array to contain the totals node in every label.
 
-  // Add the goal state.
-  first_goal_state(&state, &goal_num);
+  cout << "ENTER THE DEEP: ";
+  cin >> bound;
+  array = new int64_t[bound];
 
   // Perform depth-bounded searches with increasing depth bounds.
-  for (int i = 0; i < bound; i++){
+  for (int i = bound + 1; i >= 0; i--){
       int history = init_history;
-      int64_t numNodoAct = bounded_dfs_visit(state, i, bound, history);
-      cout << "El nivel " + convertInt(bound - i - 1) + " tiene " + convertInt(numNodoAct) + " nodos.\n";
-      oldTotalNodes = numNodoAct;
+      first_goal_state(&state, &goal_num);
+      array[i-1] = bounded_dfs_visit(state, i, bound, history);
+  }
+
+  // Print the total nodes of every label.
+
+  cout << "\n\nRESULT: \n";
+  for (int i = 0; i < bound + 1; i++)
+  {
+    totalNodes = array[i];
+    cout << "The total nodes in deep " + convertInt(i) + ": " +
+            convertInt(totalNodes - oldTotalNodes) + "\n";
+    oldTotalNodes = totalNodes;
   }
 }
 
