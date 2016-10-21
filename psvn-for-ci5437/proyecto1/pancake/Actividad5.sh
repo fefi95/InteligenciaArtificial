a367a1f14e9b592154595210bed6d938b31c7412
Stefani Castellanos <scct95@hotmail.com>
2016-10-21 18:29:48 -0400

    Actividad5.sh modificado

--- psvn-for-ci5437/proyecto1/pancake/Actividad5.sh
+++ psvn-for-ci5437/proyecto1/pancake/Actividad5.sh
@@ -2,5 +2,5 @@
 
 # filename="../../instances/pancake28.txt"
 
-make pancake28.WIDA*
-./pancake28.WIDA* $1 "pancake16_WIDA_result.csv" IDA gap 1 pancake28
+make pancake28.WIDA
+./pancake28.WIDA $1 "pancake16_WIDA_result.csv" IDA gap 1 pancake28
--- psvn-for-ci5437/proyecto1/puzzle/Actividad5.sh
+++ psvn-for-ci5437/proyecto1/puzzle/Actividad5.sh
@@ -3,4 +3,4 @@
 # filename="../../instances/15Puzzle.txt"
 
 make 15Puzzle.WIDA
-./15Puzzle.WIDA $1 "pancake16_WIDA_result.csv" WIDA manhattan 1.5 15puzzle
+./15Puzzle.WIDA $1 "15puzzle_WIDA_result.csv" WIDA manhattan 1.5 15puzzle
