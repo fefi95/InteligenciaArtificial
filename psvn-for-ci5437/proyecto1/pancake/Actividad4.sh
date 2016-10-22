cd216681900bcca46b0a0a63da29981b38e9e54a
Stefani Castellanos <scct95@hotmail.com>
2016-10-21 20:28:18 -0400

    shell script para actividad 4 y 5

--- psvn-for-ci5437/proyecto1/pancake/Actividad4.sh
+++ psvn-for-ci5437/proyecto1/pancake/Actividad4.sh
@@ -2,5 +2,5 @@
 
 filename="../../instances/pancake28.txt"
 
-make pancake28.A*
-./pancake28.A* $filename "pancake16_A*_result.csv" gap
+make pancake28.WIDA
+./pancake28.WIDA $filename "pancake28_IDA_Actividad4.csv" IDA manhattan 1.0 15puzzle
--- psvn-for-ci5437/proyecto1/pancake/Actividad5.sh
+++ psvn-for-ci5437/proyecto1/pancake/Actividad5.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
-# filename="../../instances/pancake28.txt"
+filename="../../instances/pancake28.txt"
 
 make pancake28.WIDA
-./pancake28.WIDA $1 "pancake16_WIDA_result.csv" IDA gap 1 pancake28
+echo "running WIDA* with 1.5"
+./pancake28.WIDA $filename "pancake28_1.5_Actividad5.csv" WIDA gap 1.5 pancake28
+echo "running WIDA* with 2.0"
+./pancake28.WIDA $filename "pancake28_2_Actividad5.csv" WIDA gap 2.0 pancake28
