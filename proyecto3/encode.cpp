bc771ed54b0d225e2e8efb1e6127f3fe6628549c
Stefani Castellanos <scct95@hotmail.com>
2016-12-05 20:08:01 -0400

    codificacion para el 0 y el 4

--- proyecto3/encode.cpp
+++ proyecto3/encode.cpp
@@ -10,6 +10,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
 int main(int argc, const char **argv) {
@@ -22,15 +24,61 @@ int main(int argc, const char **argv) {
 
     input.open(argv[1]);
     encode.open("encode.cnf");
-    int N, M; // dimesions (row, colmuns) of the problem
+    int N, M; // dimensions (row, colmuns) of the problem
     input >> N;
     input >> M;
+
+    //WARNING: NUMBER OF VARIABLES AND CLAUSES WITHOUT CONSIDERING THAT IS A COMPLETE CYCLE. FIX
+    int numVar = (N + 1) * N * 2;
+    int numClauses = 0;
+
+    encode << "p cnf " << numVar << " " << numClauses << std::endl;
     // std::cout << N << M << std::endl;
     char cell;
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    int val;
+    string clause; // all clauses represents a CNF problem.
+    string up, dw, lt, rt; // every segment that surrounds one cell
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
             input >> cell;
+            val = (i - 1) * M + j;
+            switch (cell) {
+                // case ' ': break;
+                case '.':
+                    break;
+                case '0':
+                        // all surrounding segments are false
+                        up = std::to_string(-val);
+                        dw = std::to_string(-(i * M + j));
+                        lt = std::to_string(-(val + i + N * (M + 1) - 1));
+                        rt = std::to_string(-(val + i + N * (M + 1)));
+                        clause  =  up + " 0\n";
+                        clause +=  dw + " 0\n";
+                        clause +=  lt + " 0\n";
+                        clause +=  rt + " 0\n";
+                    break;
+                case '1':
+                    break;
+                case '2':
+                    break;
+                case '3':
+                    break;
+                case '4':
+                        // all surrounding segments are true
+                        up = std::to_string(val);
+                        dw = std::to_string(i * M + j);
+                        lt = std::to_string(val + i + N * (M + 1) - 1);
+                        rt = std::to_string(val + i + N * (M + 1));
+                        clause  =  up + " 0\n";
+                        clause +=  dw + " 0\n";
+                        clause +=  lt + " 0\n";
+                        clause +=  rt + " 0\n";
+                    break;
+            }
             // std::cout << "cell:" << cell << std::endl;
+            encode << clause << std::endl;
+            clause = "";
         }
     }
 
