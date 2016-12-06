65a1dddbafcd544396e6e730fa9483cb5c02ddf0
Stefani Castellanos <scct95@hotmail.com>
2016-12-06 08:58:46 -0400

    intentado codificacion del 1 y del 2

--- proyecto3/encode.cpp
+++ proyecto3/encode.cpp
@@ -36,7 +36,7 @@ int main(int argc, const char **argv) {
     // std::cout << N << M << std::endl;
     char cell;
     int val;
-    string clause; // all clauses represents a CNF problem.
+    string clause = ""; // all clauses represents a CNF problem.
     string up, dw, lt, rt; // every segment that surrounds one cell
 
     for (int i = 1; i <= N; i++) {
@@ -48,6 +48,7 @@ int main(int argc, const char **argv) {
                 case '.':
                     break;
                 case '0':
+                        encode << "c rules for 0:" << std::endl;
                         // all surrounding segments are false
                         up = std::to_string(-val);
                         dw = std::to_string(-(i * M + j));
@@ -57,10 +58,75 @@ int main(int argc, const char **argv) {
                         clause +=  dw + " 0\n";
                         clause +=  lt + " 0\n";
                         clause +=  rt + " 0\n";
+                        encode << clause;
                     break;
                 case '1':
+                        up = std::to_string(val);
+                        dw = std::to_string(i * M + j);
+                        lt = std::to_string(val + i + N * (M + 1) - 1);
+                        rt = std::to_string(val + i + N * (M + 1));
+
+                        encode << "c rules for 1:" << std::endl;
+                        clause  =  up + " " + dw +  " " + lt +  " " + rt + " 0\n";
+
+                        // up segment is true and the rest are false
+                        clause +=  "-" + up + " -" + dw + " 0\n";
+                        clause +=  "-" + up + " -" + lt + " 0\n";
+                        clause +=  "-" + up + " -" + rt + " 0\n";
+
+                        // down segment is true and the rest are false
+                        clause +=  "-" + dw + " -" + lt + " 0\n";
+                        clause +=  "-" + dw + " -" + rt + " 0\n";
+
+                        // left segment is true and the rest are false
+                        clause +=  "-" + lt + " -" + rt + " 0\n";
+                        encode << clause;
                     break;
                 case '2':
+
+                        up = std::to_string(val);
+                        dw = std::to_string(i * M + j);
+                        lt = std::to_string(val + i + N * (M + 1) - 1);
+                        rt = std::to_string(val + i + N * (M + 1));
+
+                        encode << "c rules for 2:" << std::endl;
+
+                        // up and down segment are true and the rest are false
+                        clause +=  "-" + up + " -" + dw + " -"+ lt + " 0\n";
+                        clause +=  "-" + up + " -" + dw + " -"+ rt + " 0\n";
+                        clause +=  rt + " " + lt + " " + dw + " 0\n";
+                        clause +=  rt + " " + lt + " " + up + " 0\n";
+
+                        // up and left segment are true and the rest are false
+                        // clause +=  "-" + up + " -" + lt + " -"+ dw + " 0\n";
+                        clause +=  "-" + up + " -" + lt + " -"+ rt + " 0\n";
+                        // clause +=  rt + " " + dw + " " + lt + " 0\n";
+                        clause +=  rt + " " + dw + " " + up + " 0\n";
+
+                        // up and right segment are true and the rest are false
+                        // clause +=  "-" + up + " -" + rt + " -"+ dw + " 0\n";
+                        // clause +=  "-" + up + " -" + rt + " -"+ lt + " 0\n";
+                        // clause +=  lt + " " + dw + " " + rt + " 0\n";
+                        clause +=  lt + " " + dw + " " + up + " 0\n";
+
+                        // down and left segment are true and the rest are false
+                        // clause +=  "-" + dw + " -" + lt + " -"+ up + " 0\n";
+                        clause +=  "-" + dw + " -" + lt + " -"+ rt + " 0\n";
+                        // clause +=  up + " " + rt + " " + dw + " 0\n";
+                        clause +=  up + " " + rt + " " + lt + " 0\n";
+
+                        // down and right segment are true and the rest are false
+                        // clause +=  "-" + dw + " -" + rt + " -"+ up + " 0\n";
+                        // clause +=  "-" + dw + " -" + rt + " -"+ lt + " 0\n";
+                        // clause +=  up + " " + lt + " " + dw + " 0\n";
+                        // clause +=  up + " " + lt + " " + rt + " 0\n";
+
+                        // left and right segment are true and the rest are false
+                        // clause +=  "-" + lt + " -" + rt + " -"+ up + " 0\n";
+                        // clause +=  "-" + lt + " -" + rt + " -"+ dw + " 0\n";
+                        // clause +=  up + " " + dw + " " + lt + " 0\n";
+                        // clause +=  up + " " + dw + " " + rt + " 0\n";
+                        encode << clause;
                     break;
                 case '3':
                     break;
@@ -74,10 +140,10 @@ int main(int argc, const char **argv) {
                         clause +=  dw + " 0\n";
                         clause +=  lt + " 0\n";
                         clause +=  rt + " 0\n";
+                        encode << clause;
                     break;
             }
             // std::cout << "cell:" << cell << std::endl;
-            encode << clause << std::endl;
             clause = "";
         }
     }
