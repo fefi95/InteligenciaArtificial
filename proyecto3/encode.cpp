9227396f69c96adc882e1a43e98196205b71cdb2
Stefani Castellanos <scct95@hotmail.com>
2016-12-08 09:39:58 -0400

    clausulas del tipo 2 que parecen funcionar xD

--- proyecto3/encode.cpp
+++ proyecto3/encode.cpp
@@ -15,6 +15,7 @@
 using namespace std;
 
 int N, M; // dimensions (row, columns) of the problem
+int numSegm; // Number of segments of the problem
 
 /**
     Function that represents a variable for the SATLIB format
@@ -50,10 +51,8 @@ string z(int i, int j) {
     // have another enumeration. Note that the number of segments are :
     // numVertical + numHorizontal so we use that as an offset for next variables
 
-    int numHorizontal = (N + 1) * M; // number of horizontal segments
-    int numVertical = (M + 1) * N; // number of vertical segments
     int val = (i - 1) * M + j; // transforming de i,j position to a one dimensional array
-    return std::to_string(numHorizontal + numVertical + val);
+    return std::to_string(numSegm + val);
 }
 
 int main(int argc, const char **argv) {
@@ -70,113 +69,192 @@ int main(int argc, const char **argv) {
     input >> M;
 
     //WARNING: NUMBER OF VARIABLES AND CLAUSES WITHOUT CONSIDERING THAT IS A COMPLETE CYCLE. FIX
-    int numVar = (N + 1) * N * 2;
-    int numClauses = 0;
+    int numHorizontal = (N + 1) * M; // number of horizontal segments
+    int numVertical = (M + 1) * N;   // number of vertical segments
+    numSegm = numHorizontal + numVertical;
+    int numVar = numSegm; // FIX!
+    int numClauses = 0; // FIX!
 
     encode << "p cnf " << numVar << " " << numClauses << std::endl;
     // std::cout << N << M << std::endl;
     char cell;
     string clause; // all clauses represents a CNF problem.
+    char n = 'n';
+    char e = 'e';
+    char s = 's';
+    char w = 'w';
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= M; j++) {
             clause = "";
             input >> cell;
-            // Type 0 clauses (are implied because of the enumeration function q(i,j,[n,s,e,w]))
+            /******************************************************************
+                Type 0 clauses (are implied because of the enumeration function
+                q(i,j,[n,s,e,w]))
+            ******************************************************************/
 
-            // Type 1 clauses
+            /******************************************************************
+                Type 1 clauses
+            ******************************************************************/
             switch (cell) {
                 case '0':
                         encode << "c rules for 0:" << std::endl;
                         // all surrounding segments are false
-                        clause += "-" + q(i,j,'n') + " 0\n";
-                        clause += "-" + q(i,j,'e') + " 0\n";
-                        clause += "-" + q(i,j,'s') + " 0\n";
-                        clause += "-" + q(i,j,'w') + " 0\n";
+                        clause += "-" + q(i,j,n) + " 0\n";
+                        clause += "-" + q(i,j,e) + " 0\n";
+                        clause += "-" + q(i,j,s) + " 0\n";
+                        clause += "-" + q(i,j,w) + " 0\n";
                     break;
                 case '1':
                         encode << "c rules for 1:" << std::endl;
                         // a segment is true and the rest are false
-                        clause += q(i,j,'n') + " " + q(i,j,'e') + " " + q(i,j,'s') + " " + q(i,j,'w') + " 0\n";
-                        clause += "-" + q(i,j,'n') + " -" + q(i,j,'e') + " 0\n";
-                        clause += "-" + q(i,j,'n') + " -" + q(i,j,'s') + " 0\n";
-                        clause += "-" + q(i,j,'n') + " -" + q(i,j,'w') + " 0\n";
-                        clause += "-" + q(i,j,'e') + " -" + q(i,j,'s') + " 0\n";
-                        clause += "-" + q(i,j,'e') + " -" + q(i,j,'w') + " 0\n";
-                        clause += "-" + q(i,j,'s') + " -" + q(i,j,'w') + " 0\n";
+                        clause += q(i,j,n) + " " + q(i,j,e) + " " + q(i,j,s) + " " + q(i,j,w) + " 0\n";
+                        clause += "-" + q(i,j,n) + " -" + q(i,j,e) + " 0\n";
+                        clause += "-" + q(i,j,n) + " -" + q(i,j,s) + " 0\n";
+                        clause += "-" + q(i,j,n) + " -" + q(i,j,w) + " 0\n";
+                        clause += "-" + q(i,j,e) + " -" + q(i,j,s) + " 0\n";
+                        clause += "-" + q(i,j,e) + " -" + q(i,j,w) + " 0\n";
+                        clause += "-" + q(i,j,s) + " -" + q(i,j,w) + " 0\n";
                     break;
                 case '2':
                         encode << "c rules for 2:" << std::endl;
 
                         // up and down segment are true and the rest are false
-                        clause +=  "-" + q(i,j,'n') + " -" + q(i,j,'s') + " -"+ q(i,j,'w') + " 0\n";
-                        clause +=  "-" + q(i,j,'n') + " -" + q(i,j,'s') + " -"+ q(i,j,'e') + " 0\n";
-                        clause +=  q(i,j,'e') + " " + q(i,j,'w') + " " + q(i,j,'s') + " 0\n";
-                        clause +=  q(i,j,'e') + " " + q(i,j,'w') + " " + q(i,j,'n') + " 0\n";
+                        clause +=  "-" + q(i,j,n) + " -" + q(i,j,s) + " -"+ q(i,j,w) + " 0\n";
+                        clause +=  "-" + q(i,j,n) + " -" + q(i,j,s) + " -"+ q(i,j,e) + " 0\n";
+                        clause +=  q(i,j,e) + " " + q(i,j,w) + " " + q(i,j,s) + " 0\n";
+                        clause +=  q(i,j,e) + " " + q(i,j,w) + " " + q(i,j,n) + " 0\n";
 
                         // up and left segment are true and the rest are false
-                        // clause +=  "-" + q(i,j,'n') + " -" + q(i,j,'w') + " -"+ q(i,j,'s') + " 0\n";
-                        clause +=  "-" + q(i,j,'n') + " -" + q(i,j,'w') + " -"+ q(i,j,'e') + " 0\n";
-                        // clause +=  q(i,j,'e') + " " + q(i,j,'s') + " " + q(i,j,'w') + " 0\n";
-                        clause +=  q(i,j,'e') + " " + q(i,j,'s') + " " + q(i,j,'n') + " 0\n";
+                        // clause +=  "-" + q(i,j,n) + " -" + q(i,j,w) + " -"+ q(i,j,s) + " 0\n";
+                        clause +=  "-" + q(i,j,n) + " -" + q(i,j,w) + " -"+ q(i,j,e) + " 0\n";
+                        // clause +=  q(i,j,e) + " " + q(i,j,s) + " " + q(i,j,w) + " 0\n";
+                        clause +=  q(i,j,e) + " " + q(i,j,s) + " " + q(i,j,n) + " 0\n";
 
                         // up and right segment are true and the rest are false
-                        // clause +=  "-" + q(i,j,'n') + " -" + q(i,j,'e') + " -"+ q(i,j,'s') + " 0\n";
-                        // clause +=  "-" + q(i,j,'n') + " -" + q(i,j,'e') + " -"+ q(i,j,'w') + " 0\n";
-                        // clause +=  q(i,j,'w') + " " + q(i,j,'s') + " " + q(i,j,'e') + " 0\n";
-                        clause +=  q(i,j,'w') + " " + q(i,j,'s') + " " + q(i,j,'n') + " 0\n";
+                        // clause +=  "-" + q(i,j,n) + " -" + q(i,j,e) + " -"+ q(i,j,s) + " 0\n";
+                        // clause +=  "-" + q(i,j,n) + " -" + q(i,j,e) + " -"+ q(i,j,w) + " 0\n";
+                        // clause +=  q(i,j,w) + " " + q(i,j,s) + " " + q(i,j,e) + " 0\n";
+                        clause +=  q(i,j,w) + " " + q(i,j,s) + " " + q(i,j,n) + " 0\n";
 
                         // down and left segment are true and the rest are false
-                        // clause +=  "-" + q(i,j,'s') + " -" + q(i,j,'w') + " -"+ q(i,j,'n') + " 0\n";
-                        clause +=  "-" + q(i,j,'s') + " -" + q(i,j,'w') + " -"+ q(i,j,'e') + " 0\n";
-                        // clause +=  q(i,j,'n') + " " + q(i,j,'e') + " " + q(i,j,'s') + " 0\n";
-                        clause +=  q(i,j,'n') + " " + q(i,j,'e') + " " + q(i,j,'w') + " 0\n";
+                        // clause +=  "-" + q(i,j,s) + " -" + q(i,j,w) + " -"+ q(i,j,n) + " 0\n";
+                        clause +=  "-" + q(i,j,s) + " -" + q(i,j,w) + " -"+ q(i,j,e) + " 0\n";
+                        // clause +=  q(i,j,n) + " " + q(i,j,e) + " " + q(i,j,s) + " 0\n";
+                        clause +=  q(i,j,n) + " " + q(i,j,e) + " " + q(i,j,w) + " 0\n";
 
                         // down and right segment are true and the rest are false
-                        // clause +=  "-" + q(i,j,'s') + " -" + q(i,j,'e') + " -"+ q(i,j,'n') + " 0\n";
-                        // clause +=  "-" + q(i,j,'s') + " -" + q(i,j,'e') + " -"+ q(i,j,'w') + " 0\n";
-                        // clause +=  q(i,j,'n') + " " + q(i,j,'w') + " " + q(i,j,'s') + " 0\n";
-                        // clause +=  q(i,j,'n') + " " + q(i,j,'w') + " " + q(i,j,'e') + " 0\n";
+                        // clause +=  "-" + q(i,j,s) + " -" + q(i,j,e) + " -"+ q(i,j,n) + " 0\n";
+                        // clause +=  "-" + q(i,j,s) + " -" + q(i,j,e) + " -"+ q(i,j,w) + " 0\n";
+                        // clause +=  q(i,j,n) + " " + q(i,j,w) + " " + q(i,j,s) + " 0\n";
+                        // clause +=  q(i,j,n) + " " + q(i,j,w) + " " + q(i,j,e) + " 0\n";
 
                         // left and right segment are true and the rest are false
-                        // clause +=  "-" + q(i,j,'w') + " -" + q(i,j,'e') + " -"+ q(i,j,'n') + " 0\n";
-                        // clause +=  "-" + q(i,j,'w') + " -" + q(i,j,'e') + " -"+ q(i,j,'s') + " 0\n";
-                        // clause +=  q(i,j,'n') + " " + q(i,j,'s') + " " + q(i,j,'w') + " 0\n";
-                        // clause +=  q(i,j,'n') + " " + q(i,j,'s') + " " + q(i,j,'e') + " 0\n";
+                        // clause +=  "-" + q(i,j,w) + " -" + q(i,j,e) + " -"+ q(i,j,n) + " 0\n";
+                        // clause +=  "-" + q(i,j,w) + " -" + q(i,j,e) + " -"+ q(i,j,s) + " 0\n";
+                        // clause +=  q(i,j,n) + " " + q(i,j,s) + " " + q(i,j,w) + " 0\n";
+                        // clause +=  q(i,j,n) + " " + q(i,j,s) + " " + q(i,j,e) + " 0\n";
                     break;
                 case '3':
                         encode << "c rules for 3:" << std::endl;
-                        clause +=  "-" + q(i,j,'n') + " -" + q(i,j,'s') + " -"+ q(i,j,'w') + " -"+ q(i,j,'e') +" 0\n";
+                        clause +=  "-" + q(i,j,n) + " -" + q(i,j,s) + " -"+ q(i,j,w) + " -"+ q(i,j,e) +" 0\n";
 
                         // up, down and left segments are true and right is false
-                        clause +=  q(i,j,'e') + " " + q(i,j,'n') + " 0\n";
-                        clause +=  q(i,j,'e') + " " + q(i,j,'s') + " 0\n";
-                        clause +=  q(i,j,'e') + " " + q(i,j,'w') + " 0\n";
+                        clause +=  q(i,j,e) + " " + q(i,j,n) + " 0\n";
+                        clause +=  q(i,j,e) + " " + q(i,j,s) + " 0\n";
+                        clause +=  q(i,j,e) + " " + q(i,j,w) + " 0\n";
 
                         // up, down and right segments are true and left is false
-                        clause +=  q(i,j,'w') + " " + q(i,j,'n') + " 0\n";
-                        clause +=  q(i,j,'w') + " " + q(i,j,'s') + " 0\n";
-                        // clause +=  q(i,j,'w') + " " + q(i,j,'e') + " 0\n";
+                        clause +=  q(i,j,w) + " " + q(i,j,n) + " 0\n";
+                        clause +=  q(i,j,w) + " " + q(i,j,s) + " 0\n";
+                        // clause +=  q(i,j,w) + " " + q(i,j,e) + " 0\n";
 
                         // up, left and right segments are true and down is false
-                        clause +=  q(i,j,'s') + " " + q(i,j,'n') + " 0\n";
-                        // clause +=  q(i,j,'s') + " " + q(i,j,'w') + " 0\n";
-                        // clause +=  q(i,j,'s') + " " + q(i,j,'e') + " 0\n";
+                        clause +=  q(i,j,s) + " " + q(i,j,n) + " 0\n";
+                        // clause +=  q(i,j,s) + " " + q(i,j,w) + " 0\n";
+                        // clause +=  q(i,j,s) + " " + q(i,j,e) + " 0\n";
 
                         // down, left and right segments are true and up is false
-                        // clause +=  up + " " + q(i,j,'s') + " 0\n";
-                        // clause +=  q(i,j,'n') + " " + q(i,j,'w') + " 0\n";
-                        // clause +=  q(i,j,'n') + " " + q(i,j,'e') + " 0\n";
+                        // clause +=  up + " " + q(i,j,s) + " 0\n";
+                        // clause +=  q(i,j,n) + " " + q(i,j,w) + " 0\n";
+                        // clause +=  q(i,j,n) + " " + q(i,j,e) + " 0\n";
                     break;
                 case '4':
                         // all surrounding segments are true
-                        clause  =  q(i,j,'n') + " 0\n";
-                        clause +=  q(i,j,'s') + " 0\n";
-                        clause +=  q(i,j,'w') + " 0\n";
-                        clause +=  q(i,j,'e') + " 0\n";
+                        clause  =  q(i,j,n) + " 0\n";
+                        clause +=  q(i,j,s) + " 0\n";
+                        clause +=  q(i,j,w) + " 0\n";
+                        clause +=  q(i,j,e) + " 0\n";
 
                     break;
             }
+
+            /******************************************************************
+                Type 2 clauses
+            ******************************************************************/
+
+            // Upper border
+            if (i == 1) {
+                clause +=  q(i,j,n) + " -" + z(1,j) + " 0\n";
+                clause += "-" + q(i,j,n) + " " + z(1,j) + " 0\n";
+            }
+
+            // Lower border
+            else if (i == N) {
+                clause +=  q(N,j,s) + " -" + z(N,j) + " 0\n";
+                clause += "-" + q(N,j,s) + " " + z(N,j) + " 0\n";
+            }
+
+            // Left border
+            else if (j == 1) {
+                clause +=  q(i,1,w) + " -" + z(i,1) + " 0\n";
+                clause += "-" + q(i,1,w) + " " + z(i,1) + " 0\n";
+            }
+
+            // Right border
+            else if (j == N) {
+                clause +=  q(i,M,e) + " -" + z(i,M) + " 0\n";
+                clause += "-" + q(i,M,e) + " " + z(i,M) + " 0\n";
+            }
+
+            // Cells that are not in the border
+            else {
+                // z(i,j) <=> [-q(i,j,n) & z(i,j+1)] v [-q(i,j,e) & z(i+1,j)] v [-q(i,j,s) & z(i,j-1)] v [-q(i,j,w) & z(i-1,j)]
+                // renaming
+                // (~P || ((~Q && R) || (~S && T) || (~U && V) || (~W && X))) && (P || ~((~Q && R) || (~S && T) || (~U && V) || (~W && X)))
+                // to cnf:
+                // (~P | ~Q | ~S | ~U | ~W) & (~P | ~Q | ~S | ~U | X) & (~P | ~Q | ~S | V | ~W) & (~P | ~Q | ~S | V | X) & (~P | ~Q | T | ~U | ~W) & (~P | ~Q | T | ~U | X) & (~P | ~Q | T | V | ~W) & (~P | ~Q | T | V | X) & (~P | R | ~S | ~U | ~W) & (~P | R | ~S | ~U | X) & (~P | R | ~S | V | ~W) & (~P | R | ~S | V | X) & (~P | R | T | ~U | ~W) & (~P | R | T | ~U | X) & (~P | R | T | V | ~W) & (~P | R | T | V | X) & (P | Q | ~R) & (P | S | ~T) & (P | U | ~V) & (P | W | ~X)
+
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " -" + q(i,j,e) + " -" + q(i,j,s) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " -" + q(i,j,e) + " -" + q(i,j,s) + " " +  z(i-1,j) + " 0\n";
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " -" + q(i,j,e) + " " + z(i,j-1) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " -" + q(i,j,e) + " " + z(i,j-1) + " " + z(i-1,j) + " 0\n";
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " " + z(i+1,j) + " -" + q(i,j,s) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " " + z(i+1,j) + " -" + q(i,j,s) + " " + z(i-1,j) + " 0\n";
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " " + z(i+1,j) + " -" + z(i,j-1) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " -" + q(i,j,n) + " " + z(i+1,j) + " -" + z(i,j-1) + " " + z(i-1,j) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " -" + q(i,j,e) + " -" + q(i,j,s) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " -" + q(i,j,e) + " -" + q(i,j,s) + " " + z(i-1,j) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " -" + q(i,j,e) + " " + z(i,j-1) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " -" + q(i,j,e) + " " + z(i,j-1) + " " + z(i-1,j) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " " + z(i+1,j) + " -" + q(i,j,s) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " " + z(i+1,j) + " -" + q(i,j,s) + " " + z(i-1,j) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " " + z(i+1,j) + " " + z(i,j-1) + " -" + q(i,j,w) + " 0\n";
+                clause += "-" + z(i,j) + " " + q(i,j,n) + " " + z(i+1,j) + " " + z(i,j-1) + " " + z(i-1,j) + " 0\n";
+                clause += " " + z(i,j) + " " + q(i,j,n) + " -" + q(i,j,n) + " 0\n";
+                clause += " " + z(i,j) + " " + q(i,j,e) + " -" + z(i+1,j) + " 0\n";
+                clause += " " + z(i,j) + " " + q(i,j,s) + " -" + z(i,j-1) + " 0\n";
+                clause += " " + z(i,j) + " " + q(i,j,w) + " -" + z(i-1,j) + " 0\n";
+            }
+
+            /******************************************************************
+                Type 3 clauses
+            ******************************************************************/
+
+            /******************************************************************
+                Type 2 clauses
+            ******************************************************************/
+
             encode << clause;
             // std::cout << "cell:" << cell << std::endl;
         }
