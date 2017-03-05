8b588302a60053b74b2f203ba82fc854fcad027b
Piglet- <cferreira6594@gmail.com>
2017-03-05 17:12:39 -0400

    los data set para entrenar y probar

--- IA_II/proyecto2/Edward/question3.py
+++ IA_II/proyecto2/Edward/question3.py
@@ -45,40 +45,56 @@ numericClass = {"Iris-setosa": 1, "Iris-versicolor": 2, "Iris-virginica": 3}
         @param dataSetName: file's name of the dataset to use.
         @param isBinary   : the data class will be a binary o numeric class.
 """
-def readData(dataSetName, isBinary):
-    originalList = [] # initialize matrix of features without normalization.
-    varList      = [] # initialize matrix of features with normalization.
+def readData(dataSetName, isBinary, frac):
+    # originalList = [] # initialize matrix of features without normalization.
+    # varList      = [] # initialize matrix of features with normalization.
     # Open the file.
+    trainList = []
+    testList = []
     data = pd.read_csv(dataSetName, sep=",", header = None)
-    
     # Get the min and max vlues for each column. 
     minValues = []
     maxValues = []
     for i in range(0, len(data.columns)-1):
-        minValues.append(data[i].min())
-        maxValues.append(data[i].max())
+        mini = data[i].min()
+        maxi = data[i].max()
+        data1[i] = (data[i] - mini) / (maxi - mini)
+        # minValues.append(data[i].min())
+        # maxValues.append(data[i].max())
+    if (isBinary):
+        data[len(data.columns)-1].replace(binaryClass, inplace = True)
+    else: 
+        data[len(data.columns)-1].replace(numericClass, inplace = True)
+
+    data_train = data.sample(frac = frac)
+    data_test = data.drop(data_train.index)
+    for index, row in data_train.iterrows():
+        trainList.append(row.values.tolist)
+        
+    for index, row in data_test.iterrows():
+        testList.append(row.values.tolist)
 
     # Normalize the data.
-    for index, row in data.iterrows():
-        normalRow   = []
-        rowObtained = []
+    # for index, row in data.iterrows():
+    #     normalRow   = []
+    #     rowObtained = []
         
-        for i in range(0, len(row)-1):
-            normalize = (row[i] - minValues[i]) / (maxValues[i] - minValues[i])
-            normalRow.append(normalize) 
-            rowObtained.append(row[i])
+    #     for i in range(0, len(row)-1):
+    #         normalize = (row[i] - minValues[i]) / (maxValues[i] - minValues[i])
+    #         normalRow.append(normalize) 
+    #         rowObtained.append(row[i])
 
-        # Transform the data into binary class o numerical class. 
-        if (isBinary):
-            normalRow.append(binaryClass[row[len(row)-1]])
-            rowObtained.append(binaryClass[row[len(row)-1]])    
-        else:
-            normalRow.append(numericClass[row[len(row)-1]])
-            rowObtained.append(numericClass[row[len(row)-1]])
+    #     # Transform the data into binary class o numerical class. 
+    #     if (isBinary):
+    #         normalRow.append(binaryClass[row[len(row)-1]])
+    #         rowObtained.append(binaryClass[row[len(row)-1]])    
+    #     else:
+    #         normalRow.append(numericClass[row[len(row)-1]])
+    #         rowObtained.append(numericClass[row[len(row)-1]])
         
-        varList.append(normalRow)
-        originalList.append(rowObtained)
-    return { 'normalized': varList, 'original': originalList }
+    #     varList.append(normalRow)
+    #     originalList.append(rowObtained)
+    return { 'train': trainList, 'test': testList }
 
 def getConfusionMatrix(predictedValue, originalValue):
     TP = 0
@@ -108,31 +124,32 @@ def getConfusionMatrix(predictedValue, originalValue):
 def main():
 
     alpha = 0.1 # Learning rate to use.
+    frac = 0.1
 
     # Read the data.
-    dataIrisBinary  = readData('datosP2EM2017/data_iris.txt', True)
-    dataIrisNumeric = readData('datosP2EM2017/data_iris.txt', False)
+    dataIrisBinary  = readData('datosP2EM2017/data_iris.txt', True, frac)
+    dataIrisNumeric = readData('datosP2EM2017/data_iris.txt', False, frac)
 
     print "--------------------------------------------------------------------------------"
     # For split the data into i*10% for training and (100 - i*10) for test.
-    for i in range(9, 10):
+
         # Total of neuron to use in the hidden layers.
-        for j in range(4, 5):
+    for j in range(4, 5):
 
 
-            print "\n Calculando thetas para data_iris.txt usando el " + str(i/10) + " de los datos con " + str(j) + " neuronas..."
-            print "\n Creo la red. \n"
-            neuralNet = nn.NeuralNetwork(len(trainingSet[0]) - 1, j, 2)
+        print "\n Calculando thetas para data_iris.txt usando el " + str(i/10) + " de los datos con " + str(j) + " neuronas..."
+        print "\n Creo la red. \n"
+        neuralNet = nn.NeuralNetwork(len(trainingSet[0]) - 1, j, 2)
 
-            print "\n Entreno la red. \n"
-            nn.trainNetwork(neuralNet, trainingSet, alpha, maxIter, 2)  
+        print "\n Entreno la red. \n"
+        nn.trainNetwork(neuralNet, trainingSet, alpha, maxIter, 2)  
 
-            newData = []
-            for row in testSet:
-                newData.append([row[0], row[1], nn.predictNetwork(neuralNet, row)])
+        newData = []
+        for row in testSet:
+            newData.append([row[0], row[1], nn.predictNetwork(neuralNet, row)])
 
-            getConfusionMatrix(newData, testSet)
-            gf.drawPoints(newData)
+        getConfusionMatrix(newData, testSet)
+        gf.drawPoints(newData)
 # .----------------------------------------------------------------------------.
 
 if __name__ == '__main__':
