18042df8f059619276f7059cf3c46b94daaba024
Stefani Castellanos <scct95@hotmail.com>
2017-03-05 21:43:12 -0400

    estadisticas de la pregunta 3

--- IA_II/proyecto2/Edward/question3.py
+++ IA_II/proyecto2/Edward/question3.py
@@ -50,7 +50,7 @@ def readData(dataSetName, isBinary, frac):
     trainList = []
     testList = []
     data = pd.read_csv(dataSetName, sep=",", header = None)
-    # Get the min and max vlues for each column. 
+    # Get the min and max vlues for each column.
     minValues = []
     maxValues = []
     for i in range(0, len(data.columns)-1):
@@ -60,14 +60,14 @@ def readData(dataSetName, isBinary, frac):
 
     if (isBinary):
         data[len(data.columns)-1].replace(binaryClass, inplace = True)
-    else: 
+    else:
         data[len(data.columns)-1].replace(numericClass, inplace = True)
 
     data_train = data.sample(frac = frac)
     data_test = data.drop(data_train.index)
     for index, row in data_train.iterrows():
         trainList.append(row.values.tolist())
-        
+
     for index, row in data_test.iterrows():
         testList.append(row.values.tolist())
     return { 'train': trainList, 'test': testList }
@@ -77,11 +77,16 @@ def getConfusionMatrix(predictedValue, originalValue):
     FP = 0
     TN = 0
     FN = 0
-
+    correct = 0
+    incorrect = 0
     lastpos = len(originalValue[0]) - 1
     nP = 0
     nF = 0
     for i in range(0, len(predictedValue)):
+        if (abs(predictedValue[i] - originalValue[i][lastpos]) < 0.01):
+            correct += 1
+        else:
+            incorrect += 1
         if (predictedValue[i] == 1 and originalValue[i][lastpos] == 1):
             TP += 1
             nP += 1
@@ -96,10 +101,19 @@ def getConfusionMatrix(predictedValue, originalValue):
             nF += 1
         else:
             pass
-    print "TP: " + str(TP) + ", FP: " + str(FP) + ", TN: " + str(TN) + ", FN: " + str(FN)
+
+    error = float(incorrect)/len(predictedValue)
+    print "correct " + str(correct) + " incorrect "  + str(incorrect) + " error " + str(error)
+    # print "TP: " + str(TP) + ", FP: " + str(FP) + ", TN: " + str(TN) + ", FN: " + str(FN)
+    return [error, correct, incorrect]
 
 def main():
 
+    statsFB = open("datosP2EM2017/data_iris_stats_binary.csv", 'w')
+    statsFC = open("datosP2EM2017/data_iris_stats_class.csv", 'w')
+    statsFB.write("% \datos de entrenamiento, numero de neuronas, error en prueba, aciertos, fallos\n")
+    statsFC.write("% \datos de entrenamiento, numero de neuronas, error en prueba, aciertos, fallos\n")
+    
     alpha = 0.1 # Learning rate to use.
     print "--------------------------------------------------------------------------------"
     # For split the data into i*10% for training and (100 - i*10) for test.
@@ -109,15 +123,15 @@ def main():
         dataIrisNumeric = readData('datosP2EM2017/data_iris.txt', False, float(i)/10)
 
         # Total of neuron to use in the hidden layers.
-        for j in range(4, 11):    
+        for j in range(4, 11):
             print "\n Calculando thetas para data_iris.txt usando el " + str(float(i)/10) + " de los datos con " + str(j) + " neuronas..."
             print "\n Creo la red. \n"
             neuralNet = nn.NeuralNetwork(len(dataIrisBinary['train'][0]) - 1, j, 2)
             neuralNet3out = nn.NeuralNetwork(len(dataIrisNumeric['train'][0]) - 1, j, 3)
 
             print "\n Entreno la red. \n"
-            nn.trainNetwork(neuralNet, dataIrisBinary['train'], alpha, maxIter, 2)  
-            nn.trainNetwork(neuralNet3out, dataIrisNumeric['train'], alpha, maxIter, 3)  
+            nn.trainNetwork(neuralNet, dataIrisBinary['train'], alpha, maxIter, 2)
+            nn.trainNetwork(neuralNet3out, dataIrisNumeric['train'], alpha, maxIter, 3)
 
             newData = []
             for row in dataIrisBinary['test'] :
@@ -126,8 +140,11 @@ def main():
             for row in dataIrisNumeric['test'] :
                 newData3out.append(nn.predictNetwork(neuralNet3out, row))
 
-            getConfusionMatrix(newData, dataIrisBinary['test'])
-            getConfusionMatrix(newData3out, dataIrisNumeric['test'])
+            cm = getConfusionMatrix(newData, dataIrisBinary['test'])
+            statsFB.write(str(j*10) + ", " + str(i) + ", " + str(cm[0]) + ", " + str(cm[1]) + ", " + str(cm[2])+ "\n")
+
+            cm = getConfusionMatrix(newData3out, dataIrisNumeric['test'])
+            statsFC.write(str(j*10) + ", " + str(i) + ", " + str(cm[0]) + ", " + str(cm[1]) + ", " + str(cm[2])+ "\n")
 
 # .----------------------------------------------------------------------------.
 
