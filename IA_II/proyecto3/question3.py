f44c47f7171654f184d27b1127adb4c739d522ad
Edward Fernandez <edwardfernandez26@gmail.com>
2017-03-11 19:22:41 -0400

    Ya se crean las diferentes imagenes.

--- IA_II/proyecto3/question3.py
+++ IA_II/proyecto3/question3.py
@@ -23,37 +23,43 @@ from PIL import Image       #
 
 def main():
 	# Open the image.
-	originalIm = Image.open("Imagen.jpg")
+	originalIm = Image.open("Imagen2.jpg")
 	wImg, hImg = originalIm.size  
 	pix = originalIm.load()      # Get the image info.
 
-	# Create the matrix to store the pixel data.
-	# Each row will be the form: [x,y,z]
-	pixMatrix = np.zeros((wImg * hImg, 3))
+	kClusters = [2,4,8,16,32,64,128]
 
-	data = np.zeros((hImg, wImg, 3), dtype=np.uint8)
+	for k in kClusters:
+		print "Cantidad de clusters a usar: " + str(k)
+		# Create the matrix to store the pixel data.
+		# Each row will be the form: [x,y,z]
+		pixMatrix = np.zeros((wImg * hImg, 3))
 
-	i = 0
-	for w in range(0, wImg):
-		for h in range(0, hImg):
-			pixMatrix[i][0], pixMatrix[i][1], pixMatrix[i][2] = pix[w,h] 	
-			i += 1
+		data = np.zeros((hImg, wImg, 3), dtype=np.uint8)
 
-	result = km.k_means(pixMatrix, 2)
-	print result['clusters']
+		i = 0
+		for w in range(0, wImg):
+			for h in range(0, hImg):
+				pixMatrix[i][0], pixMatrix[i][1], pixMatrix[i][2] = pix[w,h] 	
+				i += 1
 
-	for i in range(0, len(result['clusters'])):
-		print result['clusters'][i][0]
+		print "Se usa k-means"
+		result = km.k_means(pixMatrix, k)
 
-	i = 0
-	for w in range(0, wImg):
-		for h in range(0, hImg):
-			data[h,w] = pixMatrix[i]
-			i += 1
-	
-	# Draw the new image.
-	newImg = Image.fromarray(data, 'RGB')
-	newImg.save('test.png')
+		j = 0
+		for w in range(0, wImg):
+			for h in range(0, hImg):
+				for i in range(0, len(result['clusters'])):
+					if np.any(result['clusters'][i] == pixMatrix[j]):	
+						data[h,w] = result['centroids'][i]
+						break
+				j += 1
+		
+
+		# Draw the new image.
+		print "Se crea la nueva imagen con " + str(k) + " colores \n"
+		newImg = Image.fromarray(data, 'RGB')
+		newImg.save('Imagen_' + str(k) + ".png")
 
 # .----------------------------------------------------------------------------.
 
